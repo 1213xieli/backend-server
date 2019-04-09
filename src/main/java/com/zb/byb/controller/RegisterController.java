13e52c38e8b88bbec0715dee99696eff33133287
chengqiupeng <1135360962@qq.com>
2019-04-09 09:38:44 +0800

    ww

---
+++ src/main/java/com/zb/byb/controller/RegisterController.java
@@ -0,0 +1,25 @@
+package com.zb.byb.controller;
+
+import com.zb.byb.entity.UserInfo;
+import com.zb.framework.common.entity.ResponseEntity;
+import io.swagger.annotations.ApiOperation;
+import net.sf.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/register")
+public class RegisterController {
+    @ApiOperation("保存注册信息")
+    @PostMapping("/save")
+    public ResponseEntity<?> register(@RequestBody UserInfo userInfo) {
+//        String json=JSONObject.fromObject(userInfo).toString();
+        return ResponseEntity.buildSuccess(null);
+    }
+    @ApiOperation("获取注册信息")
+    @GetMapping("/list")
+    public ResponseEntity<List<UserInfo>> getList(){
+        return null;
+    }
+}
