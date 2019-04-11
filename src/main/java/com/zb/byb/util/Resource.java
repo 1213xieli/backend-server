938e2be0fd0cd313a1591a1892c4eb997fb86227
chengqiupeng <1135360962@qq.com>
2019-04-11 15:45:30 +0800

    新增

--- src/main/java/com/zb/byb/util/Resource.java
+++ src/main/java/com/zb/byb/util/Resource.java
@@ -3,9 +3,9 @@ package com.zb.byb.util;
 public class Resource {
 
     /*金蝶系统登入接口地址*/
-    public static final String URL_LOGIN_TEST = "http://192.168.91.35:56898/ormrpc/services/EASLogin";//测试
+    public static final String URL_LOGIN_TEST = "http://192.168.91.5:56898/ormrpc/services/EASLogin";//测试
     /*金蝶系统业务操作接口地址*/
-    public static final String URL_TASK_TEST = "http://192.168.91.35:56898/ormrpc/services/WSCustWechatAppFacade";//测试
+    public static final String URL_TASK_TEST = "http://192.168.91.5:56898/ormrpc/services/WSCustWechatAppFacade";//测试
 
 
 }
