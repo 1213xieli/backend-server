1f310614e3aac477f17e1c100483133152e24372
chengqiupeng <1135360962@qq.com>
2019-04-11 16:15:38 +0800

    Merge branch 'master' of http://git.zhengbang.com/wechat-farmer-service/backend-server
    # Conflicts:
    #	src/main/java/com/zb/byb/controller/BalanceRecordController.java

--- src/main/java/com/zb/byb/controller/BalanceRecordController.java
+++ src/main/java/com/zb/byb/controller/BalanceRecordController.java
@@ -1,14 +1,14 @@
 package com.zb.byb.controller;
 
 import com.zb.byb.entity.BalanceRecord;
-import com.zb.byb.entity.DeathApply;
+
 import com.zb.framework.common.entity.ResponseEntity;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
