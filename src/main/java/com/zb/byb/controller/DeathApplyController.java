ad4c87d937289e41e52dcc256cd590e9fd7eb052
chengqiupeng <1135360962@qq.com>
2019-04-13 08:41:43 +0800

    Merge branch 'master' of http://git.zhengbang.com/wechat-farmer-service/backend-server
    # Conflicts:
    #	src/main/java/com/zb/byb/controller/BalanceRecordController.java

--- src/main/java/com/zb/byb/controller/DeathApplyController.java
+++ src/main/java/com/zb/byb/controller/DeathApplyController.java
@@ -1,8 +1,14 @@
 package com.zb.byb.controller;
 
+
 import com.zb.byb.entity.DeathApply;
+import com.zb.byb.service.DeathApplyService;
 import com.zb.framework.common.entity.ResponseEntity;
 import io.swagger.annotations.ApiOperation;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,32 +21,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/deathApply")
 public class DeathApplyController {
+    @Autowired
+    private DeathApplyService deathApplyService;
     @ApiOperation("保存死亡申报")
     @PostMapping("/save")
-    public ResponseEntity<?> deathApply(@RequestBody DeathApply deathApply) {
-        DeathApply info = new DeathApply();
-        info.setDieDate(new Date());
-        info.setBatchId("32xieli");
-        info.setApplyDieNum(33);
-        info.setId("ffdcie");
-        info.setDayAge(31);
-        info.setWei(32.32);
-        info.setJing(65.434);
-        return ResponseEntity.buildSuccess(info);
+    public ResponseEntity<?> deathApply(@RequestBody DeathApply deathApply, HttpServletRequest request) {
+        //获取userId
+        String userId=(String) request.getSession().getAttribute("userId");
+        try {
+            String backData= deathApplyService.deathApply(deathApply,userId);
+            return ResponseEntity.buildSuccess(backData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.build(500,"服务器出错");
+        }
     }
     @ApiOperation("获取死亡申报记录")
     @GetMapping("/list")
-    public ResponseEntity<List<DeathApply>> getList(){
-        List<DeathApply> list = new ArrayList<>();
-        DeathApply info = new DeathApply();
-        info.setDieDate(new Date());
-        info.setBatchId("32xieli");
-        info.setApplyDieNum(33);
-        info.setId("ffdcie");
-        info.setDayAge(31);
-        info.setWei(32.32);
-        info.setJing(65.434);
-        list.add(info);
-        return ResponseEntity.buildSuccess(list);
+    public ResponseEntity<List<DeathApply>> getList(HttpServletRequest request){
+        //获取userId
+        String userId=(String) request.getSession().getAttribute("userId");
+        try {
+            String backData= deathApplyService.getDeathApplyRecord(userId);
+            String data=JSONObject.fromObject(backData).getString("data");
+            //转成list
+            JSONArray fromObject = JSONArray.fromObject(backData);
+            List<DeathApply> list1 = fromObject.toList(fromObject,DeathApply.class);
+            ResponseEntity<List<DeathApply>> resp=new ResponseEntity<>();
+            resp.setData(list1);
+            return resp;
+            //return ResponseEntity.buildSuccess(batchRecordService.viewBatchRecord(batchId,openId));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.build(500,"服务器出错");
+        }
+
     }
 }
--- src/main/java/com/zb/byb/controller/FeedRecordController.java
+++ src/main/java/com/zb/byb/controller/FeedRecordController.java
@@ -1,10 +1,16 @@
 package com.zb.byb.controller;
 
+
 import com.zb.byb.entity.FeedRecord;
+import com.zb.byb.service.FeedRecordService;
 import com.zb.framework.common.entity.ResponseEntity;
 import io.swagger.annotations.ApiOperation;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,24 +21,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/feedRecord")
 public class FeedRecordController {
+    @Autowired
+    FeedRecordService feedRecordService;
     @ApiOperation("保存饲喂记录")
     @PostMapping("/save")
-    public ResponseEntity<?> saveFeedRecord(@RequestBody FeedRecord feedRecord) {
-        FeedRecord info = new FeedRecord();
-        info.setBatchId("32fv");
-        info.setFeedDate(new Date());
-        info.setId("xxxieli");
-        return ResponseEntity.buildSuccess(info);
+    public ResponseEntity<?> saveFeedRecord(@RequestBody FeedRecord feedRecord, HttpServletRequest request) {
+        String userId=(String) request.getSession().getAttribute("userId");
+        try {
+            return ResponseEntity.buildSuccess(feedRecordService.addFeedRecord(feedRecord,userId));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.build(500,"服务出现问题");
+        }
     }
     @ApiOperation("获取饲喂记录")
     @GetMapping("/list")
-    public ResponseEntity<List<FeedRecord>> getList(){
-        List<FeedRecord> list = new ArrayList<>();
-        FeedRecord info = new FeedRecord();
-        info.setBatchId("32fv");
-        info.setFeedDate(new Date());
-        info.setId("xxxieli");
-        list.add(info);
-        return ResponseEntity.buildSuccess(list);
+    public ResponseEntity<List<FeedRecord>> getList(HttpServletRequest request){
+        //获取userId
+        String userId=(String) request.getSession().getAttribute("userId");
+        try {
+            String backData= feedRecordService.queryFeedRecord(userId);
+            String data= JSONObject.fromObject(backData).getString("data");
+            //转成list
+            JSONArray fromObject = JSONArray.fromObject(backData);
+            List<FeedRecord> list1 = fromObject.toList(fromObject,FeedRecord.class);
+            ResponseEntity<List<FeedRecord>> resp=new ResponseEntity<>();
+            resp.setData(list1);
+            return resp;
+            //return ResponseEntity.buildSuccess(batchRecordService.viewBatchRecord(batchId,openId));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.build(500,"服务器出错");
+        }
+
+
     }
 }
