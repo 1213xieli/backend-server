a524fccffc43ab5b32c51b8dbac8d36e3f9eb49a
gurenguo <gurenguo@zhengbang.com>
2019-04-25 11:02:46 +0800

    rollback

--- src/main/java/com/zb/byb/config/WebMvcConfig.java
+++ src/main/java/com/zb/byb/config/WebMvcConfig.java
@@ -3,15 +3,10 @@ package com.zb.byb.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * springboot访问本地路径并获取图片等静态资源文件的url
- */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private OpenIdInterceptor openIdInterceptor;
@@ -20,10 +15,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter implements WebMvcConfi
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //registry.addInterceptor(openIdInterceptor);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
