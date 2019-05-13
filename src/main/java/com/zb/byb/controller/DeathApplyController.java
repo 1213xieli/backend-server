43ec88905e1d616bbf6fb0ce7eb1a2ca841b6109
man_qq <415992946@qq.com>
2019-05-13 19:27:19 +0800

    1

--- src/main/java/com/zb/byb/controller/DeathApplyController.java
+++ src/main/java/com/zb/byb/controller/DeathApplyController.java
@@ -47,6 +47,17 @@ public class DeathApplyController {
             List<FileEntry> list=new ArrayList<>();
             for (String serverId : deathApply.getServerIds()) {
                 File file = HttpConnectionUtil.downloadWxImg(serverId);
+                try {
+                    if (file == null) {
+                        throw new Exception("获取文件失败");
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    Message message = new Message();
+                    message.setCode(C.parseStr(Commonconst.FailStatus));
+                    message.setMessage(e.getMessage());
+                    return ResponseEntity.build(Commonconst.FailStatus, message);
+                }
                 String base64Img = Image2Base64Util.fileToBase64(file);
                 FileEntry fileEntry = new FileEntry();
                 fileEntry.setImgContent(base64Img);
